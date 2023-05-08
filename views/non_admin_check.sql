dcb32e7f135a3d67cefcffdf47c0f3c7f7ded272
Bruce Jacobo <116692210+BruceJ18@users.noreply.github.com>
2023-05-07 18:40:49 -0700

    commit-10: Update non_admin_check.sql

--- views/non_admin_check.sql
+++ views/non_admin_check.sql
@@ -4,8 +4,10 @@ use clients_meetings_db;
 -- and having simple contact and business info (annual revenue).
 
 create view non_admin_check as
-select * from clients_businesses cb
-join clients c using (client_id)
-where cb.annual_revenue = cb.annual_revenue
-and c.email = c.email
-and c.phone_number = c.phone_number;
+select 
+	cb.annual_revenue, 
+  c.email,
+  c.phone_number
+from clients_businesses cb
+join clients c using (client_id);
+
